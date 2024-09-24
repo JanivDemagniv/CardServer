35d2e685c9f426f141eabffb885c90e1365e67ac
JanivDemagniv <janivjry@gmail.com>
2024-09-24 14:53:22 +0300

    cros policy update

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,7 +2,7 @@ const cors = require('cors');
 
 
 const corsMiddleWare = cors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5173']
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5173', 'https://fullstackcardproject.onrender.com/']
 })
 
 
