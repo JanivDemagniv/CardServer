7fea33e85cb2fa905b70ba8473eee3dd0857a7c4
JanivDemagniv <janivjry@gmail.com>
2024-10-04 12:28:08 +0300

    cardsRestController sytax update

--- cards/routes/cardsRestController.js
+++ cards/routes/cardsRestController.js
@@ -11,99 +11,99 @@ router.get("/", async (req, res) => {
         const cards = await getCards();
         res.send(cards);
     } catch (error) {
-        handleError(res, 400, error.message)
-    }
+        handleError(res, 400, error.message);
+    };
 });
 
 router.get('/my-cards', auth, async (req, res) => {
     try {
-        const userInfo = req.user
-        const id = req.user._id
+        const userInfo = req.user;
+        const id = req.user._id;
+
         if (!userInfo.isBusiness) {
             handleError(res, 403, 'Only buissness member have cards');
         }
         const cards = await getMyCards(id);
-        res.send(cards)
+        res.send(cards);
     } catch (error) {
-        handleError(res, 400, error.message)
-    }
-})
-
-
-router.post("/", auth, async (req, res) => {
-    try {
-        const userInfo = req.user
-        if (!userInfo.isBusiness) {
-            handleError(res, 403, 'you are not authuraize')
-        }
-
-        const errorMessage = validateCard(req.body);
-        if (errorMessage !== '') {
-            return handleError(res, 400, 'Validate Error:' + errorMessage)
-        }
-
-        let card = await normalizeCard(req.body, userInfo._id)
-        card = await createCard(card);
-        res.status(201).send(card);
-    } catch (error) {
-        handleError(res, 400, error.message)
-    }
+        handleError(res, 400, error.message);
+    };
 });
+
 
 router.get('/:id', async (req, res) => {
     try {
-        const { id } = req.params
+        const { id } = req.params;
         const card = await getCard(id);
-        res.send(card)
+        res.send(card);
     } catch (error) {
-        handleError(res, 400, error.message)
-    }
+        handleError(res, 400, error.message);
+    };
+});
+
+router.post("/", auth, async (req, res) => {
+    try {
+        const userInfo = req.user;
+        if (!userInfo.isBusiness || !userInfo.isAdmin) {
+            handleError(res, 403, 'you are not authuraize');
+        };
+
+        const errorMessage = validateCard(req.body);
+        if (errorMessage !== '') {
+            return handleError(res, 400, 'Validate Error:' + errorMessage);
+        };
+
+        let card = await normalizeCard(req.body, userInfo._id);
+        card = await createCard(card);
+        res.status(201).send(card);
+    } catch (error) {
+        handleError(res, 400, error.message);
+    };
 });
 
 
 router.put('/:id', auth, async (req, res) => {
     try {
-        const userInfo = req.user
+        const userInfo = req.user;
         const { id } = req.params;
         const newCard = req.body;
         const fullCardFromDb = await getCard(id);
 
         if (userInfo._id != fullCardFromDb.user_id && !userInfo.isAdmin) {
-
-            return handleError(res, 403, 'Athorization error: you are no allowed to edit the card')
-        }
+            return handleError(res, 403, 'Athorization error: you are no allowed to edit the card');
+        };
 
         const errorMessage = validateCard(newCard);
         if (errorMessage !== "") {
-            return handleError(res, 400, 'Validation Error' + errorMessage)
-        }
+            return handleError(res, 400, 'Validation Error' + errorMessage);
+        };
 
         let card = await normalizeCard(newCard, userInfo._id);
         card = await updateCard(id, card);
-        res.send(card)
+        res.send(card);
     } catch (error) {
-        handleError(res, 400, error.message)
-    }
+        handleError(res, 400, error.message);
+    };
 });
 
 router.patch('/:id', auth, async (req, res) => {
     try {
         const { id } = req.params;
-        const userId = req.user._id
-        let card = await updateLike(id, userId)
-        res.send(card)
+        const userId = req.user._id;
+        let card = await updateLike(id, userId);
+        res.send(card);
     } catch (error) {
-        handleError(res, 400, error.message)
-    }
+        handleError(res, 400, error.message);
+    };
 });
 
 router.patch('/:id', async (req, res) => {
     try {
         let card = await updateBizNumber(req.body._id, req.body);
-        res.send(card)
+        res.send(card);
     } catch (error) {
-        handleError(res, 400, error.message)
-    }
+        handleError(res, 400, error.message);
+    };
 });
 
 router.delete('/:id', auth, async (req, res) => {
@@ -111,14 +111,16 @@ router.delete('/:id', auth, async (req, res) => {
         let { id } = req.params;
         let userInfo = req.user;
         let fullCardFromDb = await getCard(id);
+
         if (userInfo._id != fullCardFromDb.user_id && !userInfo.isAdmin) {
-            handleError(res, 403, 'You Are not allowed to Delete')
-        }
+            handleError(res, 403, 'You Are not allowed to Delete');
+        };
+
         let card = await deleteCard(id);
         res.send(card);
     } catch (error) {
-        handleError(res, 400, error.message)
-    }
+        handleError(res, 400, error.message);
+    };
 });
 
 module.exports = router;
